0ee5bb569831900bf0ff8edc2a6926cd4589a3cc
hud9925 <dan.hu@mail.utoronto.ca>
2025-05-02 23:38:33 +0900

    move secrets to env vars; add safe settings_example

--- config/settings_example.py
+++ config/settings_example.py
@@ -1,3 +1,13 @@
-SQLALCHEMY_URI = (
-   "database_url";
-)
+"""
+Non‑secret defaults
+"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()                      # reads .env locally 
+
+KIWIS_BASE_URL = "https://waterdata.grandriver.ca/KiWIS/KiWIS"
+TIMEZONE       = "America/Toronto"
+
+SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_URI"]   # required
+SQLALCHEMY_TRACK_MODIFICATIONS = False
